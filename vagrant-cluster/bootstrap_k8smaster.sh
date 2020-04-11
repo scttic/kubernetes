831a56b82fb5a42420dc864312655dd3ddec7f19
scttic <contato@sttic.com>
2020-04-11 20:26:54 -0300

    Criação dos arquivos de deploy do cluster

--- vagrant-cluster/bootstrap_k8smaster.sh
+++ vagrant-cluster/bootstrap_k8smaster.sh
@@ -2,7 +2,7 @@
 
 # Initialize Kubernetes
 echo "[TASK 1] Initialize Kubernetes Cluster"
-kubeadm init --apiserver-advertise-address=192.168.100.100 --pod-network-cidr=172.16.0.0/16 >> /root/kubeinit.log 2>/dev/null
+kubeadm init --apiserver-advertise-address=192.168.100.100 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
 
 # Copy Kube admin config
 echo "[TASK 2] Copy kube admin config to Vagrant user .kube directory"
@@ -11,9 +11,10 @@ cp /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown -R vagrant:vagrant /home/vagrant/.kube
 
 # Deploy flannel network
-echo "[TASK 3] Deploy Calico network"
-su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml"
+echo "[TASK 3] Deploy Flannel network"
+# su - vagrant -c "kubectl create -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml"
+su - vagrant -c "kubectl apply -f https://raw.githubusercontent.com/scttic/kubernetes/master/vagrant-cluster/kube-flannel.yml"
 
 # Generate Cluster join command
 echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
-kubeadm token create --print-join-command > /tmp/joincluster.sh
+kubeadm token create --print-join-command > /tmp/joincluster.sh 2>/dev/null
